70c79ebed049841410d8575efa45ce8e64cdb33e
mozkilic <mmozkilic92@gmail.com>
2019-01-17 15:16:27 +0300

    Create test project with python 2.7

---
+++ faceDetectFromImg.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('4.jpg')
+body_cascade = cv2.CascadeClassifier("frontalface.xml")
+
+griton = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+bodies = body_cascade.detectMultiScale(griton, 1.1, 4)
+
+for (x, y, w, h) in bodies:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 1)
+
+cv2.imshow('body', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
